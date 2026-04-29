031d82f25ede549a4a2bf517478e68ef6897c5f1
Jan <jh2824@att.com>
2026-04-29 11:47:39 +0200

    Update from Lab

--- Fork.c
+++ Fork.c
@@ -4,19 +4,24 @@
 #include <sys/wait.h>
 
 int main() {
-    int pid;
-    printf("Start for Fork with pid: %d\n", getpid());
+    int pid, x;
+    x = 10;
+    printf("Start for Fork with pid: %d, x = %d\n", getpid(), x);
     pid = fork();
     if (pid < 0) {
 	fprintf(stderr, "Fork failed\n");
 	exit(1);
     }
     else if (pid == 0) {
-	printf("I am the Child with pid: %d\n", getpid());
+	    x = 20;
+	printf("I am the Child with pid: %d, x = %d\n", getpid(), x);
     }
     else {
-	wait(NULL);
-	printf("I am the Parent with pid: %d, my Child has pid: %d\n", getpid(), pid);
+	//wait(NULL);
+	x = 15;
+	printf("I am the Parent with pid: %d, my Child has pid: %d, x = %d\n", getpid(), pid, x);
     }
+    wait(NULL);
+    printf("Ending Fork with pid: %d, x = %d\n", getpid(), x);
     return 0;
 }
